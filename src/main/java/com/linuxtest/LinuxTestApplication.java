ce38f55e3b683c89c760ce543b358ef8d761f978
dongadong <3588377558@qq.com>
2023-01-09 23:50:33 +0800

    Initial commit

---
+++ src/main/java/com/linuxtest/LinuxTestApplication.java
@@ -0,0 +1,13 @@
+package com.linuxtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinuxTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinuxTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/linuxtest/LinuxTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linuxtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinuxTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
